dedc719b233056ef0de4d8b0ff8c3640958fd1f5
ngonthier <nicolas.gonthier@ign.fr>
2024-08-09 15:16:06 +0200

    for running pix2pix on FLAIR INC

---
+++ create_flair_correct_format.py
@@ -0,0 +1,205 @@
+import glob
+import pathlib
+import os
+from PIL import Image
+import numpy as np
+from osgeo import gdal
+from tqdm import tqdm
+import pandas as pd
+
+LUT = [
+    {"color": "#db0e9a", "class": "building"},
+    {"color": "#938e7b", "class": "pervious surface"},
+    {"color": "#f80c00", "class": "impervious surface"},
+    {"color": "#a97101", "class": "bare soil"},
+    {"color": "#1553ae", "class": "water"},
+    {"color": "#194a26", "class": "coniferous"},
+    {"color": "#46e483", "class": "deciduous"},
+    {"color": "#f3a60d", "class": "brushwood"},
+    {"color": "#660082", "class": "vineyard"},
+    {"color": "#55ff00", "class": "herbaceous vegetation"},
+    {"color": "#fff30d", "class": "agricultural land"},
+    {"color": "#e4df7c", "class": "plowed land"},
+    {"color": "#3de6eb", "class": "swimming pool"},
+    {"color": "#ffffff", "class": "snow"},
+    {"color": "#8ab3a0", "class": "clear cut"},
+    {"color": "#6b714f", "class": "mixed"},
+    {"color": "#c5dc42", "class": "ligneous"},
+    {"color": "#9999ff", "class": "greenhouse"},
+    {"color": "#000000", "class": "other"}
+]
+
+
+
+def convert_to_seg(image_array, lut):
+    def hex_to_tuple(hex):
+        h = hex.lstrip('#')
+        return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+    for i in range(image_array.shape[0]):
+        for j in range(image_array.shape[1]):
+            class_id = image_array[i, j][0]
+            
+            color = hex_to_tuple(lut[class_id-1]["color"]) if class_id < len(lut) else (0,0,0)
+
+            image_array[i, j] = color
+
+    # Convert the NumPy array back to an image and save it to a file
+    return image_array
+
+
+def convert_seg(input_path, output_path, LUT):
+    input_tif = gdal.Open(input_path)
+    rgb_data = np.zeros((input_tif.RasterYSize, input_tif.RasterXSize, 3), dtype=np.uint8)
+
+    rgb_data[..., 0] = input_tif.GetRasterBand(1).ReadAsArray()
+    rgb_data = convert_to_seg(rgb_data, LUT)
+
+    # Close input TIFF file
+    input_tif = None
+
+    img = Image.fromarray(rgb_data)
+    img.save(output_path)
+
+def convert_image(input_path, output_path):
+    input_tif = gdal.Open(input_path)
+    rgb_data = np.zeros((input_tif.RasterYSize, input_tif.RasterXSize, 3), dtype=np.uint8)
+
+    for i in range(3): # Keep only RGB
+        rgb_data[..., i] = input_tif.GetRasterBand(i+1).ReadAsArray()
+
+    # Close input TIFF file
+    input_tif = None
+
+    img = Image.fromarray(rgb_data)
+    img.save(output_path)
+
+def convert_msk(input_path, output_path):
+    im = Image.open(msk)
+    imarray = np.array(im)
+    # Need to remove the other classes and to convert to color 256 RGB 
+    print(np.shape(imarray),np.unique(imarray))
+    nb_mask += 1
+
+def convert_dataset_pix2pix_format(path_dataset='\\\\store\\store-DAI\\projets\\ocs\\dataset\\dp014_V1-2_FLAIR19_RVBIE',
+                                   output_path='D:\data\PixtoPix_FLAIR',max_number_img=20,
+                                   path_csv_files='D:\data\FLAIR-INC'):
+    """
+    Create folder /path/to/data with subfolders A and B. A and B should each have their own subfolders train, val, test, etc. 
+    In /path/to/data/A/train, put training images in style A. In /path/to/data/B/train, put the corresponding images in style B.
+    Repeat same for other data splits (val, test, etc)."""
+
+    path_train_A = os.path.join('A','train')
+    path_train_B = os.path.join('B','train')
+    path_test_A = os.path.join('A','test')
+    path_test_B = os.path.join('B','test')
+
+    train_set = os.path.join(path_csv_files,'TRAIN_FLAIR-INC 1.csv')
+    test_set_path = os.path.join(path_csv_files,'TEST_FLAIR-INC 1.csv')
+
+    test_set = pd.read_csv(test_set_path,names=['img','msk'])
+    test_set['img'] = test_set['img'].apply(lambda x: x.split('/')[-1])
+    test_set['img'] = test_set['img'].apply(lambda x: x.split('.')[0])
+    list_test_img = test_set['img'].to_list()
+
+    pathlib.Path(os.path.join(output_path,path_train_A)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join(output_path,path_train_B)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join(output_path,path_test_A)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join(output_path,path_test_B)).mkdir(parents=True, exist_ok=True)
+    aerial_tif = glob.glob(os.path.join(path_dataset,"*","*","img","*.tif"))
+    mask_tif = glob.glob(os.path.join(path_dataset,"*","*","msk","*.tif"))
+
+    number_image = 0
+    print('Number of images :',len(aerial_tif))
+
+    for img, msk in tqdm(zip(aerial_tif, mask_tif)):
+        
+        if "D032_2016" in img: # Zone a ignorer 
+            continue
+
+        if number_image > max_number_img:
+            break
+        print(number_image,img)
+        basename = os.path.basename(img)
+        filename_img = os.path.basename(img).split('.')[0]
+        filename_msk = os.path.basename(msk).split('.')[0]
+
+        #number = filename_img.split('_')[1]
+        if img in list_test_img: 
+            folder_A = path_test_A
+            folder_B = path_test_B
+        else:
+            folder_A = path_train_A
+            folder_B = path_train_B
+
+
+        base_dir_A = os.path.join(output_path,folder_A)
+        base_dir_B = os.path.join(output_path,folder_B)
+
+        mask_out = f"{base_dir_A}/{filename_img}.png"
+        image_out = f"{base_dir_B}/{filename_img}.png"
+        #json_out = f"{base_dir}/{number}.json" # can be used for the prompt of pix2pixTurbo non ? 
+        #percentages_out = f"{base_dir}/PCT_{number}.json"
+
+        #meta = metadata[filename_img]
+
+        convert_seg(msk, mask_out, LUT)
+        convert_image(img, image_out)
+        number_image += 1
+
+
+
+
+def copy_files_format_for_pix2pix(path_dataset='\\\\store\\store-DAI\\projets\\ocs\\dataset\\dp014_V1-2_FLAIR19_RVBIE',output_path='D:\data\PixtoPix_FLAIR',max_number_img=2):
+    """
+    Create folder /path/to/data with subfolders A and B. A and B should each have their own subfolders train, val, test, etc. 
+    In /path/to/data/A/train, put training images in style A. In /path/to/data/B/train, put the corresponding images in style B.
+    Repeat same for other data splits (val, test, etc)."""
+    
+    
+    # Besoin de charger les images dans le test set / celle dans le train set et celle à ignorer 
+
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    domains = [f for f in os.listdir(path_dataset)]
+    
+    nb_img = 0
+    nb_mask = 0
+
+    for domain in domains:
+        print(domain) 
+        if domain=='': # Domain à laisser de côté 
+            continue
+        path_domain= os.path.join(path_dataset, domain)
+        zones = [f for f in os.listdir(path_domain)]
+        for zone in zones:
+            path_zone = os.path.join(path_domain, zone,'msk')
+            list_msks = glob.glob(os.path.join(path_zone,'*.tif'))
+            print(list_msks)
+            for msk in list_msks: 
+                if nb_mask < max_number_img:
+                    print(msk)
+                    im = Image.open(msk)
+                    imarray = np.array(im)
+                    # Need to remove the other classes and to convert to color 256 RGB 
+                    print(np.shape(imarray),np.unique(imarray))
+                    nb_mask += 1
+            path_aerial = os.path.join(path_domain, zone,'img')
+            list_imgs = glob.glob(os.path.join(path_aerial,'*.tif'))
+            for img in list_imgs: 
+                if nb_img < max_number_img:
+                    print(img)
+                    im = Image.open(img)
+                    imarray = np.array(im)
+                    print(np.shape(imarray),np.unique(imarray))
+                    nb_img += 1
+
+
+
+
+if __name__ == '__main__':  
+
+    #copy_files_format_for_pix2pix()
+    #convert_dataset_pix2pix_format()
+    convert_dataset_pix2pix_format(path_dataset='/gpfsscratch/rech/tel/commun/dataset_ocs/dp014_V1-2_FLAIR19_RVBIE',
+                                   output_path='/lustre/fsn1/projects/rech/abj/ujq24es/dataset/PixtoPix_FLAIR',max_number_img=20000000000,
+                                   path_csv_files='/lustre/fsn1/projects/rech/abj/ujq24es/dataset/FLAIR-INC')
---
+++ evaluate_quant_metrics.py
@@ -0,0 +1,78 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import glob
+import numpy as np
+import cv2
+import sys
+import argparse
+import json
+import torch_fidelity
+import pathlib
+import shutil
+
+
+"""
+This code is based on https://github.com/Mid-Push/Decent/
+"""
+
+def create_folder_per_modality(path_results='\results\flair_pix2pix\test_latest\images'):
+    fakes = glob.glob(os.path.join(path_results, '*_fake_B.png'))
+    reals = glob.glob(os.path.join(path_results, '*_real_B.png'))
+    pathlib.Path(os.path.join(path_results,'reals')).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join(path_results,'fakes')).mkdir(parents=True, exist_ok=True)
+    for fake in fakes:
+        src = fake
+        dst = os.path.join(path_results,'fakes',os.path.basename(fake))
+        shutil.copyfile(src, dst)
+    for real in reals:
+        src = real
+        head, tail = os.path.split(os.path.split(real)[0])
+        dst = os.path.join(path_results,'reals',os.path.basename(real))
+        shutil.copyfile(src, dst)
+
+
+def eval_pix2pix(method, path):
+
+    create_folder_per_modality(path_results=path)
+    real_path = os.path.join(path,'reals')
+    fake_path = os.path.join(path,'fakes')
+
+    eval_dict = { 'method': method}
+
+    # 'kid_subset_size': 50, 'kid_subsets': 10 pour le vrai cas 
+    eval_args = {'isc': True, 'fid': True, 'kid': True, 'kid_subset_size': 1, 'kid_subsets': 1, 'verbose': False, 'cuda': True}
+    metric_dict_AB = torch_fidelity.calculate_metrics(input1=real_path, input2=fake_path, **eval_args)
+    print('metric_dict_AB',metric_dict_AB)
+    eval_dict['ISC'] = metric_dict_AB['inception_score_mean']
+    eval_dict['FID'] = metric_dict_AB['frechet_inception_distance']
+    eval_dict['KID'] = metric_dict_AB['kernel_inception_distance_mean']*100.
+    print('[*] evaluation finished!')
+    
+    #print('rmse: {0}, acc5: {1}, acc10: {2}, pFPR: {3}, iFPR: {4}'.format(RMSE, acc5, acc10, pFPR100, iFPR))
+    return eval_dict
+
+
+def parse_args():
+    # Training settings
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--path', type=str, help='path to the generated and gt images')
+    parser.add_argument('--output_path', type=str, help='path to save the evaluation results', default='.')
+    parser.add_argument('--dataset', type=str, default='FLAIR')
+    parser.add_argument('--method', type=str, default='pix2pix')
+    parser.add_argument('--gpu', type=str, default='0')
+
+    args = parser.parse_args()
+    print(' '.join(sys.argv))
+    print(args)
+    return args
+
+def main():
+    args = parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    eval_metrics = eval_pix2pix(args.method, args.path)
+    with open(os.path.join(args.output_path, args.dataset + '_result.json'), 'w') as fp:
+        json.dump(eval_metrics, fp)
+    
+
+if __name__ == '__main__':
+    main()
